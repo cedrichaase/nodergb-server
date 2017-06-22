0db0a030552b5777fe437a9aaefa32eae962d20b
Cedric Haase <cedric@sineband.de>
2017-06-22 22:23:46 +0200

    Add service discovery proof of concept, update config

--- app.ts
+++ app.ts
@@ -121,3 +121,47 @@ udp.on('listening', () => {
 udp.bind(1337);
 
 /* endregion udp-server */
+
+
+/* region discovery */
+
+
+const udpDiscoverRecv = dgram.createSocket('udp4');
+
+udpDiscoverRecv.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    udpDiscoverRecv.close();
+});
+
+udpDiscoverRecv.on('message', (message, rinfo) => {
+    message = message.toString().trim();
+
+    console.log(`Received discover response: ${message}`);
+});
+
+udpDiscoverRecv.on('listening', () => {
+    let address = udpDiscoverRecv.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+
+
+
+    // send hello to broadcast
+    const udpDiscoverSend = dgram.createSocket('udp4');
+
+    udpDiscoverSend.on('listening', () => {
+        udpDiscoverSend.setBroadcast(true);
+
+        const port = 1341;
+        const msg = 'hello';
+
+        udpDiscoverSend.send(Buffer.from(msg), port, '255.255.255.255', (err) => {
+            console.log(`sent broadcast to port ${port}`);
+        });
+    });
+    udpDiscoverSend.bind();
+
+});
+
+udpDiscoverRecv.bind(1340);
+
+/* endregion discovery */
